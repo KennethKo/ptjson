4911f45397e3a3d49cd6cba885d3eb6ccfb6e551
KennethKoPC <31635054+KennethKo@users.noreply.github.com>
2018-06-09 22:30:37 -0700

    Heuristics and more speculation.

--- ptjson.js
+++ ptjson.js
@@ -227,6 +227,15 @@ if (typeof PTJSON.unshallowify !== "function") {
 }
 
 if (typeof PTJSON.prototypify !== "function") {
+
+    PTJSON._interCount = function _interCount(set1, set2) {
+        var ret = 0;
+        set1.forEach(function(val) {
+            if (set2.has(val)) { ret++;}
+        });
+        return ret;
+    }
+
     PTJSON.deprototypify = function prototypify(object, options) {
         "use strict";
 // Iterate over an object or array, searching for common sets of property values that could be clustered into a common prototypes.
@@ -250,36 +259,99 @@ if (typeof PTJSON.prototypify !== "function") {
             }
         }));
         // build attribute map on key and key-value attribute references
-        var attrsByObj = new WeakMap()
-        var objsByAttr = new WeakMap();
+        var attrsByObj = new Map()
+        var objsByAttr = new Map(); // TODO weakmap? Review all the new maps to see which ones don't need iterators or size tracking
         PTJSON._iterate(object, (function (value) {
             if (value.__proto__ !== null && value.__proto__ !== Object.prototype) {
                 // TODO impl support for pre-existing __proto__. Treat like a super attribute (which MUST match in sets)
                 return;
             }
-            var attrs = Object.keys().map(key => key + " : " + objectRefs.has(value) ? objectRefs[value] : value);
-            attrsByObj.set(value, new WeakSet(attrs));
+            var attrs = Object.keys(value).map(key => key + " : " + objectRefs.has(value) ? objectRefs[value] : value);
+            attrsByObj.set(value, new Set(attrs));
             attrs.forEach( function (attr) {
                 if (!objsByAttr.has(attr)) {
-                    objsByAttr.set(keyVal, new WeakSet());
+                    objsByAttr.set(keyVal, new Set());
                 }
                 objsByAttr[attr].add(value);
             })
         }));
+        // build a set of heuristic object scores to evaluate overall object connectedness
+        var objScores = new Map();
+        attrsByObj.forEach(function (attrs, obj)) {
+            var score = 0;
+            attrs.forEach(function(attr) {
+                var attrUsage = objsByAttr.get(attr).size;
+                if (attrUsage >= options.minProtoUsage) {
+                    score += attrUsage;
+                }
+            });
+            objScores.set(obj, score);
+        }
+        // build a set of heuristic attr scores to evaluate overal attr connectedness (for candidate attrs)
+        var attrScores = new Map();
+        objsByAttr.forEach(function (objs, attr)) {
+            if (objs.size < options.minProtoUsage) {
+                continue;
+            }
+            var score = 0;
+            objs.forEach(function(obj) {
+                score += objScores.get(obj);
+            }
+            attrScores.set(attr, score);
+        }
+
 
 // Second step, cluster common key-value attributes greedily. Memoize visited objects (do we memoize attrs or objs)?
+        var mergedAttrs = new WeakSet();    // all objects that have already been merged into a prototype candidate TODO perhaps this should be merged attrs?
         // calculate intersection of each attribute's objectSet
-        Object.keys(objsByAttr).forEach(function(attr) {
-            if (objsByAttr.get(attr).length < options.minProtoUsage) {
+        objsByAttr.forEach(function(candidateObjects, attr, objsByAttr) {
+            if (mergedAttrs.has(attr)) {
+                return;
+            }
+            mergedAttrs.add(attr);
+            if (candidateObjects.size < options.minProtoUsage) {
                 continue;
             }
 
-            // TODO iter into each obj for candidate neighbors? What does merge candidate look like?
-            // TODO what does the candidate score look like? Neighbords have a certain number of common objects, but (A^B) ^ C may be different from (A^C) v (A^C).
-            // TODO  Traditional clustering doesn't quite make sense here, because each attr added to a cluster potentially *shrinks* its footprint, rather than increasing it.
-            // TODO  That's the key to engineer around. I'd rather not n*k^2 to do it here though.
+            // 2.1 Find an initial <minProtoUsage> candidate set to merge together from the given objects that have the most overlap
+            var protoCandidate = {
+                candidateObjects: [],
+                attrSet: new Set(),
+                parentProto: Object.prototype
+            }
+            // among the common objects, look for another attr with as many common objects as possible
+            candidateObjects.forEach(function(candidateObject) {
+                if (mergedObjects.has(candidateObject)) {   // O(k*n)
+                    return;
+                }
+                var candidateAttrs = attrsByObj.get(candidateObject);
+                PTJSON._interCount(attrSet, candidateAttrs);    // which attrSet do we start with?
+                // TODO does this even make sense? we should be validating on object sets, no?
+                // TODO this feels like it's getting the attrs of the union of objects, not the intersection. I guess the intersection comes later?
 
-            // TODO ah fuck. This is so fucking hard. Let's simplify, and break out the hard parts later
+                // TODO - in the abstract - pick two attributes with significant overlap. If you can't find one, or if you can't find a third, simply move on to the next attr.
+                // TODO - the choice of what constitutes significant overlap is the problem. There are scenarios in which you're trading off more attrs for fewer objects, and vice versa. The goal should probably be attr commonality*obj commonality, which is sum of local obj set sizes.
+                // TODO the maps don't track "object commonality" - how often an object shares an attr with another. Perhaps that's a gap.
+                // TODO  -If I had object commonality scores.... what would I do with them? Would I use them to judge the value of attrs?
+                // TODO
+
+
+                // TODO count map? What do I do with a count map? Attrs that are equal or close to this attr?
+                // TODO each attr already has a friggin count. What exactly does this localized count do for my grouping? Attr -> objs -> attrs
+                // TODO what do I do when I manually create a proto? I look at a common attr, and I look for other common attrs among them. Ones that are common get added to the set, ones that aren't arent.
+                // TODO find the most common attr, then the second most common. Go naive greedy and validate after.
+            });
+
+            // 2.2 Add the other objects to the candidate.
+            // 2.2a If the merge w/ the intersection does not reduce it, add it immediately.
+            // 2.2b If it does reduce it, spawn a parent prototype if it's a true candidate prototype set.
+
+            // TODO iter into each obj for candidate neighbors? What does merge candidate look like? Do we add them back to the map or something?
+            // TODO what does the candidate score look like? Neighbords have a certain number of common objects, but (A^B) ^ C may be different from (A^C) v (A^C).
+
+            // TODO Where do we collect these candidates? Do we need to reference them again in the future? If we need to spawn child-prototypes in the future, maybe - but how could you tell?
+            // TODO Note that there's not actually a uniqueness requirement on attributes. Multiple protos could have idential attributes. Maybe I'm back to clustering objects.
+            // TODO Maintaining references to existing prototypes seems important and intuitive.
 
         });
 
